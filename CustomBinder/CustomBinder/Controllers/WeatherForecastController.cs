cef9ef1496db92960e38f4c19a0f09c2e0a3e4be
lianggx <lianggx@foxmail.com>
2020-10-26 11:15:42 +0800

    CustomBinder

---
+++ CustomBinder/CustomBinder/Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomBinder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 3).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return GetWeatherForecast();
+        }
+
+
+        [Produces("application/xml")]
+        [HttpGet("xml")]
+        public IEnumerable<WeatherForecast> Xml()
+        {
+            return GetWeatherForecast();
+        }
+
+
+        [Produces("text/weather")]
+        [HttpGet("weather")]
+        public IEnumerable<WeatherForecast> Weather()
+        {
+            return GetWeatherForecast();
+        }
+    }
+}
---
+++ CustomBinder/CustomBinder/WeatherOutputFormatter.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomBinder
+{
+    public class WeatherOutputFormatter : TextOutputFormatter
+    {
+        private readonly static Type WeatherForecastType = typeof(WeatherForecast);
+        public WeatherOutputFormatter()
+        {
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+            SupportedMediaTypes.Add("text/weather");
+        }
+
+        public override bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.ObjectType == WeatherForecastType || context.Object is WeatherForecast || context.ObjectType.GenericTypeArguments[0] == WeatherForecastType)
+            {
+                return base.CanWriteResult(context);
+            }
+
+            return false;
+        }
+
+
+        private string WriterText(IEnumerable<WeatherForecast> weathers)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var wealther in weathers)
+            {
+                builder.Append(WriterText(wealther));
+            }
+            return builder.ToString();
+        }
+
+        private string WriterText(WeatherForecast weather) => $"date={WebUtility.UrlEncode(weather.Date.ToString())}&temperatureC={weather.TemperatureC}&temperatureF={weather.TemperatureF}&summary={WebUtility.UrlEncode(weather.Summary)}";
+
+        public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (selectedEncoding == null)
+            {
+                throw new ArgumentNullException(nameof(selectedEncoding));
+            }
+
+            string text = string.Empty;
+
+            if (context.ObjectType == WeatherForecastType)
+                text = WriterText(context.Object as WeatherForecast);
+            else if (context.ObjectType.GenericTypeArguments[0] == WeatherForecastType)
+                text = WriterText(context.Object as IEnumerable<WeatherForecast>);
+
+            if (string.IsNullOrEmpty(text))
+            {
+                await Task.CompletedTask;
+            }
+
+            var response = context.HttpContext.Response;
+            await response.WriteAsync(text, selectedEncoding);
+        }
+    }
+}
